a00444606e68ccff9f45165d7f6f638b71414d2d
NoahMaktabi <NoahMaktabi@users.noreply.github.com>
2025-01-14 10:37:11 +0000

    Deploying to gh-pages from @ Vitec-EEASBDL/vitec.webpos@0dbaf0f8d67fc0bf1082f1c94d892616a99f9148 🚀

--- _content/Vitec.WebPos.Shared.Ui/js/gridLayout.js
+++ _content/Vitec.WebPos.Shared.Ui/js/gridLayout.js
@@ -15,6 +15,13 @@ export function gridLayout(cols, rows) {
         margin: 10,
         staticGrid: true
     });
+    grid.on("added removed change", function (e, items) {
+        let str = "";
+        items.forEach(function (item) {
+            str += " (x,y)=" + item.x + "," + item.y;
+        });
+        console.log(e.type + " " + items.length + " items:" + str);
+    });
 }
 
 function enableGridLayout(edit) {
@@ -22,10 +29,20 @@ function enableGridLayout(edit) {
 }
 
 function saveGrid() {
-    console.log(grid.save());
-    return grid.save();
+    if (grid) {
+        return grid.save();
+    }
+
+    return null;
+}
+function clearGrid() {
+    if (grid) {
+         grid.destroy(false);
+         grid = null;
+    }
 }
 
+window.clearGrid = clearGrid;
 window.saveGrid = saveGrid;
 window.gridLayout = gridLayout;
 window.enableGridLayout = enableGridLayout;
